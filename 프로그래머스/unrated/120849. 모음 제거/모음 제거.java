6d396e41922f79c1f1cb2cd3867cb8813e44af4e
Jinwook Kim <wodnrtkfka@naver.com>
2023-11-23 17:54:56 +0900

    [level unrated] Title: 모음 제거, Time: 0.04 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string) {
+         StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < my_string.length(); i++) {
+           
+            if (my_string.charAt(i) != 'a' && my_string.charAt(i) != 'e' && my_string.charAt(i) != 'i' && my_string.charAt(i) != 'o' && my_string.charAt(i) != 'u') {
+                answer.append(my_string.charAt(i));
+            }
+            
+        }return answer.toString();
+
+    }
+}
