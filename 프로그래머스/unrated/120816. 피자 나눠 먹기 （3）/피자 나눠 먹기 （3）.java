48e1984146000ef55d08f083cea4145eaea1d962
Jinwook Kim <wodnrtkfka@naver.com>
2023-11-24 16:02:28 +0900

    [level unrated] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/unrated/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,15 +1,14 @@
 class Solution {
     public int solution(int slice, int n) {
-           int answer = 0;
+        
         int morePizza = 0;
 
         if (n % slice != 0) {
             morePizza = 1;
         }
 
-        answer = n/slice +morePizza;
 
+        return n/slice +morePizza;
 
-        return answer;
     }
 }
