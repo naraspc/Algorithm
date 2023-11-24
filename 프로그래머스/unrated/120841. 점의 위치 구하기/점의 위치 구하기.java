b69e28b7fedf91f65c5c26d2c1bf41d60b730b3d
Jinwook Kim <wodnrtkfka@naver.com>
2023-11-24 16:08:47 +0900

    [level unrated] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.java
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int[] dot) {
-        int left_negative = 0;
+      int left_negative = 0;
         int left_positive = 0;
         int right_negative = 0;
         int right_positive = 0;
         int answer = 0;
-        
+
         for (int i = 0; i < dot.length; i++) {
 
             if (i == 0) {
@@ -22,16 +22,22 @@ class Solution {
                 }
             }
         }
-        
+        return (findPositon(left_negative,left_positive,right_negative,right_positive));
+
+
+
+
+    }
+    public int findPositon(int left_negative,int left_positive, int right_negative, int right_positive) {
+        int answer = 0;
         if (left_negative > 0 && right_negative > 0) {
             answer = 3;
         } else if (left_positive > 0 && right_positive > 0) {
             answer = 1;
         } else if (left_negative > 0 && right_positive > 0) {
             answer = 2;
-        } else 
+        } else
             answer = 4;
-        
 
         return answer;
     }
