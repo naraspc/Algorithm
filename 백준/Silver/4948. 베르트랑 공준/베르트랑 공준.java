2ccc439564806ebdd33cf214c2ebf7650d1cae7b
Jinwook Kim <wodnrtkfka@naver.com>
2023-11-24 09:43:10 +0900

    [Silver II] Title: 베르트랑 공준, Time: 292 ms, Memory: 29352 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -0,0 +1,38 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+
+       while (true) {
+           int n = Integer.parseInt(sc.readLine());
+           if (n == 0) break;
+           boolean[] prime = new boolean[2*n+1];
+
+           int cnt = 0;
+           Arrays.fill(prime, true);
+
+           prime[0] = false;
+           prime[1] = false;
+
+
+           for (int i = 2; i < prime.length; i++) {
+               if (!prime[i]) continue;
+               for (int j = i * 2; j < prime.length; j += i) {
+                   prime[j] = false;
+               }
+
+           }
+
+           for (int i = n + 1; i <= 2 * n; i++) {
+               if (prime[i]) cnt++;
+           }
+           System.out.println(cnt);
+       }
+    }
+
+}
