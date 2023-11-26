04d62014e117d28bda6a41984b396d6dbb3432de
Jinwook Kim <wodnrtkfka@naver.com>
2023-11-27 01:30:56 +0900

    [Bronze II] Title: 피보나치 수, Time: 132 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -7,17 +7,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(sc.readLine());
+        if (num <2) {
+            System.out.println(num);
+        } else {
+            int[] dp = new int[num + 1];
+            dp[0] = 0;
+            dp[1] = 1;
 
-        int[] dp = new int[num+1];
+            for (int i = 2; i < dp.length; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
 
-        dp[0] = 0;
-        dp[1] = 1;
-
-        for (int i = 2; i < dp.length; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            System.out.println(dp[num]);
         }
-
-        System.out.println(dp[num]);
-
     }
 }
