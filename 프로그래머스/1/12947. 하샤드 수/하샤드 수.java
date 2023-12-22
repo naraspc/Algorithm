940b85ce1a6313576a9f9f231315208cbd88b07c
Jinwook Kim <wodnrtkfka@naver.com>
2023-12-22 10:09:28 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,13 @@
+class Solution {
+  public boolean solution(int x) {
+       
+        String strX = Integer.toString(x);
+        int sum = 0;
+        
+        for (int i = 0; i < strX.length(); i++) {
+            sum += Character.getNumericValue(strX.charAt(i));
+        }
+        
+        return x % sum == 0;
+    }
+}
